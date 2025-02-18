e22c6bdd869580e7e3d3d0776bb147fb5673d804
Roma <khairom2005@gmail.com>
2025-02-18 10:21:00 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import LanguageSwitcherTest from './test/componentsTest/LanguageSwitcherTest';
 import Footer from './test/componentsTest/Footer';
+import Hader from './components/Hader';
 
 function App() {
 	return (
 		<div>
+			<Hader/>
 			<Footer />
 			<LanguageSwitcherTest/>
+			
 		</div>
 	);
 }
