d5ab12a9b5dddf500c263fcaebb99484103409bb
Roma <khairom2005@gmail.com>
2025-02-20 11:48:55 +0500

    7 steps

--- src/components/Hader/Header.tsx
+++ src/components/Hader/Header.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.css";
+import Logo from "./Logo/Logo";
+import Navigation from "./navigation/Navigation";
+import Social from "./social/Social";
 
 function Header() {
   return (
-    <header className='header'>
-      
+    <header className={styles.backGraund}>
+      <div className={styles.header}>
+        <Logo />
+        <Navigation />
+        <Social />
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
