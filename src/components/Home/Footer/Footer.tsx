a07c4b8ee19fe59b75b2cd04e0d6ee8d49e1d5c4
Roma <khairom2005@gmail.com>
2025-04-15 13:03:16 +0500

    pages home done

--- src/components/Home/Footer/Footer.tsx
+++ src/components/Home/Footer/Footer.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import style from "./Footer.module.css";
+import iconInstagram from "../../../assets/ic-instagram.png";
+import iconWhatApp from "../../../assets/ic-whatsapp.png";
+import map from "../../../assets/map.png";
 
 export default function Footer() {
   return (
-    <div>
-      
+    <div className={style.container}>
+      <h1></h1>
+      <div className={style.items}>
+        <div className={style.item}>
+            <p className={style.text1}>Phone</p>
+            <p className={style.text2}>+7 (499) 350-66-04</p>
+        </div>
+        <div className={style.item2}>
+          <p className={style.text1}>Socials</p>
+          <img src={iconInstagram} alt="" />
+          <img src={iconWhatApp} alt="" />
+        </div>
+      </div>
+      <div className={style.items}>
+        <div className={style.item}>
+          <p className={style.text1}>Address</p>
+          <p className={style.text2}>Dubininskaya Ulitsa, 96, Moscow, Russia, 115093</p>
+        </div>
+        <div className={style.item2}>
+          <p className={style.text1}>Working Hours</p>
+          <p className={style.text2}>24 hours a day</p>
+        </div>
+      </div>
+      <div className={style.map}>
+        <img src={map} alt="" />
+      </div>
     </div>
-  )
+  );
 }
--- src/components/Home/Sales/Sales.tsx
+++ src/components/Home/Sales/Sales.tsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from "react";
+import style from "./Sales.module.css";
 
 export default function Sales() {
   return (
-    <div>
-      
+    <div className={style.container}>
+      <div className={style.title}>
+        <h2>Sale</h2>
+      </div>
+      <div className={style.items}>
+        <div className={style.item}>
+          <div className={style.discount1}>
+            <p>-50%</p>
+          </div>
+
+          <div className={style.itemText}>
+            <p>Decorative forged bridge</p>
+            <div className={style.itemPrice}>
+              <p>$500</p>
+              <p className={style.itemPriceDiscount}>$1000</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.item}>
+          <div className={style.discount2}>
+            <p>-22%</p>
+          </div>
+          <div className={style.itemText}>
+            <p>Decorative forged bridge</p>
+            <div className={style.itemPrice}>
+              <p>$500</p>
+              <p className={style.itemPriceDiscount}>$1000</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.item}>
+          <div className={style.discount3}>
+            <p>-10%</p>
+          </div>
+          <div className={style.itemText}>
+            <p>Decorative forged bridge</p>
+            <div className={style.itemPrice}>
+              <p>$500</p>
+              <p className={style.itemPriceDiscount}>$1000</p>
+            </div>
+          </div>
+        </div>
+        <div className={style.item}>
+          <div className={style.discount4}>
+            <p>-10%</p>
+          </div>
+          <div className={style.itemText}>
+            <p>Decorative forged bridge</p>
+            <div className={style.itemPrice}>
+              <p>$500</p>
+              <p className={style.itemPriceDiscount}>$1000</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
