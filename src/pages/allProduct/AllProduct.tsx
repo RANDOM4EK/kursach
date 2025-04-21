14fa0a815c06206136b6f037b51d360f1b1559cb
random4ek <khairom2005@gmail.com>
2025-04-21 21:45:37 +0500

    allProduct filter

--- src/pages/allProduct/AllProduct.tsx
+++ src/pages/allProduct/AllProduct.tsx
@@ -1,48 +1,76 @@
-import React from "react";
+import { useState } from "react";
 import style from "./AllProduct.module.css";
 import Header from "../../components/Home/Header/Header";
 import Footer from "../../components/Home/Footer/Footer";
 import obj from "../../productsObj/ProductsObj.json";
 
 export default function AllProduct() {
+  const [minPrice, setMinPrice] = useState(Number);
+  const [maxPrice, setMaxPrice] = useState(Number);
+  const [showDiscounted, setShowDiscounted] = useState(false);
+  const filteredProducts = obj.filter((item) => {
+    const isWithinPriceRange =
+      (!minPrice || item.price >= minPrice) &&
+      (!maxPrice || item.price <= maxPrice);
+    const isDiscountedMatch = showDiscounted ? item.isDiscounted : true;
+    return isWithinPriceRange && isDiscountedMatch;
+  });
+
   return (
     <>
-      <Header></Header>
+      <Header/>
       <div className={style.container}>
-        <h1></h1>
+        <h1>Tools and equipment</h1>
         <div className={style.filter}>
           <p>Price</p>
-          <input type="number" placeholder="from" />
-          <input type="number" placeholder="to" />
+          <input
+            type="number"
+            placeholder="от"
+            value={minPrice}
+            onChange={(e) => setMinPrice(Number(e.target.value))}
+          />
+          <input
+            type="number"
+            placeholder="до"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
           <p>Discounted items</p>
-          <input type="checkbox" />
-          <p>Sorted</p>
-          <input type="text" placeholder="by default" />
+          <input
+            type="checkbox"
+            checked={showDiscounted}
+            onChange={(e) => setShowDiscounted(e.target.checked)}
+          />
         </div>
         <div className={style.products}>
-        {obj.map((item) => {
-        return (
-          <div key={item.id} className={style.item}>
-            <div className={style.item}>
-              <div className={`flex flex-row-reverse bg-no-repeat bg-top-center w-[316px] h-[284px]`}
-              style={{ backgroundImage: `url(${item.img})` }}>
-                <p className={style.discount}>{item.discount}%</p>
-              </div>
-              <div className={style.itemText}>
-                <p>{item.title}</p>
-                <div className={style.itemPrice}>
-                  <p>${item.price}</p>
-                  <p className={style.itemPriceDiscount}>${item.priceDiscounted}</p>
+          {filteredProducts.map((item) => (
+            <div key={item.id} className={style.item}>
+              <div className={style.item}>
+                <div
+                  className={`flex flex-row-reverse bg-no-repeat bg-top-center w-[316px] h-[284px]`}
+                  style={{ backgroundImage: `url(${item.img})` }}
+                >
+                  {item.isDiscounted && (
+                    <p className={style.discount}>{item.discount}%</p>
+                  )}
+                </div>
+                <div className={style.itemText}>
+                  <p>{item.title}</p>
+                  <div className={style.itemPrice}>
+                    <p>${item.price}</p>
+                    {item.isDiscounted && (
+                      <p className={style.itemPriceDiscount}>
+                        ${item.priceDiscounted}
+                      </p>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          ))}
         </div>
       </div>
-
-      <Footer></Footer>
+      <Footer/>
     </>
   );
 }
